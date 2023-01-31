3fe68ea0b5c530a404f1d0a32cee533e4285bac5
MianMianSHAN <mianmian-shan@hotmail.com>
2023-01-31 11:27:51 +0100

    #squelette du projet vide
    Créations des 3 microservices vides

---
+++ my-finder/src/main/java/com/example/projet_Note/ProjetNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_Note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetNoteApplication.class, args);
+	}
+
+}
---
+++ my-finder/src/test/java/com/example/projet_Note/ProjetNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_Note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
